ef9792715d379e35c00f2ac0da00e7481061327b
nel215 <otomo.yuhei@gmail.com>
2013-01-21 19:22:22 +0900

    配列使って高速化

--- 0264A/src/Main.scala
+++ 0264A/src/Main.scala
@@ -6,15 +6,20 @@ import java.io.PrintWriter
 object Main {
 
   def main(args: Array[String]): Unit = {
-    @tailrec
-    def rec(ind:Int,seq:List[Char],left:List[Int],right:List[Int]):List[Int]=seq match{
-      case List() => return left.reverse++right
-      case _ if(seq.head=='l') => return rec(ind+1,seq.tail,left,ind+:right)
-      case _ => return rec(ind+1,seq.tail,ind+:left,right)
-    }
     val br = new BufferedReader(new InputStreamReader(System.in))
-    val pw = new PrintWriter(new OutputStreamWriter(System.out))
-    rec(1,br.readLine().toList,List(),List()).map(pw.println(_))
+    val pw = new PrintWriter(System.out)
+    val inp = br.readLine()
+    var res = new Array[Int](inp.length())
+    @tailrec
+    def rec(ind:Int,seq:List[Char],left:Int,right:Int):Unit={
+      if(!seq.isEmpty)seq.head match{
+      	case 'l' => {res(right) = ind;rec(ind+1,seq.tail,left,right-1)}
+      	case 'r' => {res(left)  = ind;rec(ind+1,seq.tail,left+1,right)}
+      }
+    }
+    rec(1,inp.toList,0,inp.length()-1)
+    res.map(pw.println(_))
+    pw.flush()
   }
 
 }
